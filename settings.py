065f417ee7274253e6ed77213420cece128c523f
animage <animage@animage.(null)>
2012-07-14 09:56:05 +0700

    ipfw pipe fix

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ DATABASES = {
         'NAME': 'probill',                      # Or path to database file if using sqlite3.
         'USER': 'probill',                      # Not used with sqlite3.
         'PASSWORD': 'probill',                  # Not used with sqlite3.
-        'HOST': 'ramms.animage.me',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
