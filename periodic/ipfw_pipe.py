1593bc10c7b8c68d1de3eed9e5b7ce0c91d952f7
animage <animage@animage-hp-book.(none)>
2013-01-08 22:19:18 +0700

    IFPW hard work

--- periodic/ipfw_pipe.py
+++ periodic/ipfw_pipe.py
@@ -1,8 +1,6 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
-import default_periodic
-import random
 from settings import *
 from probill.billing.models import *
 from probill.nas.models import *
@@ -113,7 +111,16 @@ class IpfwPipes(IpfwObject):
 
 
     def add(self,id,arg):
-        self.get('pipe %s config bw %sKbit/s queue %s gred 0.002/%s/%s/0.1' % (id,arg,IPFW_QUEUE_SIZE,IPFW_QUEUE_SIZE/2,IPFW_QUEUE_SIZE))
+        if id % 2:
+            mask = 'src-ip'
+        else:
+            mask = 'dst-ip'
+        self.get('pipe %s config bw %sKbit/s queue %s gred 0.002/%s/%s/0.1 mask %s 0xffffffff' % (id,
+                                                                                                  arg,
+                                                                                                  IPFW_QUEUE_SIZE,
+                                                                                                  IPFW_QUEUE_SIZE/2,
+                                                                                                  IPFW_QUEUE_SIZE,
+                                                                                                  mask))
 
     def remove(self,id):
         self.get('pipe %s delete' % id)
@@ -146,12 +153,12 @@ def main():
     QOS_TABLE = IPFW_MIN_TABLE + 1
     ipfw_tables = {IPFW_MIN_TABLE:{},QOS_TABLE:{},QOS_TABLE+1:{},IPFW_NAT_TABLE:{}}
     ipfw_rules_in = {
-        IPFW_END_IN - IPFW_RULE_STEP*2 : 'queue tablearg ip from table(%s) to any' % QOS_TABLE,
+        IPFW_END_IN - IPFW_RULE_STEP*2 : 'pipe tablearg ip from table(%s) to any' % QOS_TABLE,
         IPFW_END_IN - IPFW_RULE_STEP: 'allow ip from table(%s) to any' % IPFW_MIN_TABLE,
         IPFW_END_IN : 'deny ip from any to any',
         }
     ipfw_rules_out = {
-        IPFW_END_OUT - IPFW_RULE_STEP*2 : 'queue tablearg ip from any to table(%s)' % (QOS_TABLE + 1),
+        IPFW_END_OUT - IPFW_RULE_STEP*2 : 'pipe tablearg ip from any to table(%s)' % (QOS_TABLE + 1),
         IPFW_END_OUT - IPFW_RULE_STEP: 'allow ip from any to table(%s)' % IPFW_MIN_TABLE,
         IPFW_END_OUT : 'deny ip from any to any',
         }
@@ -202,18 +209,18 @@ def main():
     qos_maps = {}
     queue_map = {}
     queue_map_id = 1
-    table_number = QOS_TABLE + 1
-    rule_offset = IPFW_RULE_STEP
-    qacs = QosAndCost.objects.all().exclude(qos_speed=0)[:100]
-    for qac in qacs:
-        rule_offset += IPFW_RULE_STEP
-        table_number += 2
-        qos_maps[qac.id] = table_number
-        ipfw_tables[table_number] = {}
-        ipfw_tables[table_number+1] = {}
-        nets = ','.join([str(f.network) for f in qac.subnets.all()])
-        ipfw_rules_in[rule_offset + IPFW_START_IN] = 'queue tablearg ip from table(%s) to %s' % (table_number,nets)
-        ipfw_rules_out[rule_offset + IPFW_START_OUT] = 'queue tablearg ip from %s to table(%s)' % (nets,table_number+1)
+#    table_number = QOS_TABLE + 1
+#    rule_offset = IPFW_RULE_STEP
+#    qacs = QosAndCost.objects.all().exclude(qos_speed=0)[:100]
+#    for qac in qacs:
+#        rule_offset += IPFW_RULE_STEP
+#        table_number += 2
+#        qos_maps[qac.id] = table_number
+#        ipfw_tables[table_number] = {}
+#        ipfw_tables[table_number+1] = {}
+#        nets = ','.join([str(f.network) for f in qac.subnets.all()])
+#        ipfw_rules_in[rule_offset + IPFW_START_IN] = 'queue tablearg ip from table(%s) to %s' % (table_number,nets)
+#        ipfw_rules_out[rule_offset + IPFW_START_OUT] = 'queue tablearg ip from %s to table(%s)' % (nets,table_number+1)
 
 
     for account in Account.objects.filter(active=True):
@@ -225,28 +232,28 @@ def main():
             ipfw_tables[QOS_TABLE][account.CIDR] = queue_map[speed_id]
             ipfw_tables[QOS_TABLE+1][account.CIDR] = queue_map[speed_id]+1
 
-        qacs = account.tariff.qac_class.all().exclude(qos_speed=0)
-        if qacs:
-            for qac in qacs:
-                speed_id = '_'.join([str(qac.qos_speed),str(qac.id)])
-                if speed_id not in queue_map:
-                    queue_map_id += 2
-                    queue_map[speed_id] = queue_map_id
-                ipfw_tables[qos_maps[qac.id]][account.CIDR] = queue_map[speed_id]
-                ipfw_tables[qos_maps[qac.id]+1][account.CIDR] = queue_map[speed_id]
+#        qacs = account.tariff.qac_class.all().exclude(qos_speed=0)
+#        if qacs:
+#            for qac in qacs:
+#                speed_id = '_'.join([str(qac.qos_speed),str(qac.id)])
+#                if speed_id not in queue_map:
+#                    queue_map_id += 2
+#                    queue_map[speed_id] = queue_map_id
+#                ipfw_tables[qos_maps[qac.id]][account.CIDR] = queue_map[speed_id]
+#                ipfw_tables[qos_maps[qac.id]+1][account.CIDR] = queue_map[speed_id]
 
     pipe_std = {}
     queue_std = {}
     for x in queue_map:
         pipe_std[queue_map[x]] = int(x.split('_')[0])
         pipe_std[queue_map[x]+1] = int(x.split('_')[0])
-        queue_std[queue_map[x]] = queue_map[x]
-        queue_std[queue_map[x]+1] = queue_map[x]+1
+        #queue_std[queue_map[x]] = queue_map[x]
+        #queue_std[queue_map[x]+1] = queue_map[x]+1
 
     Pipes = IpfwPipes()
     Pipes.check(pipe_std)
-    Queues = IpfwQueues()
-    Queues.check(queue_std)
+    #Queues = IpfwQueues()
+    #Queues.check(queue_std)
 
 
 
