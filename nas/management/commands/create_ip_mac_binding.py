9a1db1df312824e7df7a50db9e000160c7978880
animage <admin@animage.me>
2013-09-22 01:33:52 +0700

    freebsd-ip-mac refactory v3

--- nas/management/commands/create_ip_mac_binding.py
+++ nas/management/commands/create_ip_mac_binding.py
@@ -23,9 +23,12 @@ class Command(BaseCommand):
                         and arp_dict[str(acc.ip)]['status'] == 'permanent'
                 else:
                     return False, False
-
+            account_ips = []
             for interface in IPInterface.objects.filter(iface__nas=nas):
-                for acc in Account.objects.filter(ip__in=interface.network).exclude(mac=''):
+                for acc in Account.objects.filter(ip__in=interface.network):
+                    account_ips.append(str(acc.ip))
+                    if not acc.mac:
+                        continue
                     has_arp = check_arp(acc)
                     if DEBUG:
                         print acc.ip, acc.mac, has_arp
@@ -34,9 +37,9 @@ class Command(BaseCommand):
                     if has_arp[0]:
                         del arp_dict[str(acc.ip)]
             for ip in arp_dict:
-                if arp_dict[ip]['status'] == 'permanent':
+                if arp_dict[ip]['status'] == 'permanent' and ip in account_ips:
                     if DEBUG:
-                        print 'Delete old mac'
+                        print 'Delete old mac', ip, arp_dict[ip]
                     nas.exec_command(' '.join([SUDO_PATH, 'arp', '-nd', ip]))
 
 
