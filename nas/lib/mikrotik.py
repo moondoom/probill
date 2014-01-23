68f98e6b317477880a9454969a30a6d42ad4659e
animage <me@animage.me>
2014-01-24 00:04:07 +0700

    MikDHCP

--- nas/lib/mikrotik.py
+++ nas/lib/mikrotik.py
@@ -8,11 +8,13 @@ class Firewall:
     address_list_name = "PROBILL_USERS"
     qos_re = re.compile(r'(?ms).*?(\d+)\s*;;;.*?name="([^"]*)"\s*target=(\S*).*?limit-at=([^/]*)/(\S*).*?')
     access_re = re.compile(r'\s+(\d+)\s+(\S*)\s+([\.\da-f:]+)\s+')
+    dhcp_re = re.compile(r'(\d+)\s+([\.\da-f:]+)\s+([\dA-Fa-f:]+)\s+')
 
     def __init__(self, nas):
         self.nas = nas
 
     def sync_access(self):
+        print "ACCESS"
         stdin, stdout, stderr = self.nas.exec_command("/ip firewall address-list print without-paging "
                                                       "where list={}".format(self.address_list_name))
         mik_table = self.access_re.findall(stdout.read())
@@ -22,10 +24,11 @@ class Firewall:
 
         for subnet in IPInterface.objects.filter(iface__nas=self.nas):
             for account in Account.objects.filter(active=True, tariff__isnull=False, ip__in=subnet.network):
-                print 'Add', account
+
                 if str(account.ip) in mik_dict:
                     del mik_dict[str(account.ip)]
                 else:
+                    print 'Add', account
                     self.nas.exec_command('/ip firewall address-list '
                                           'add list={} address={}'.format(self.address_list_name, account.ip))
         for ip in mik_dict:
@@ -33,6 +36,8 @@ class Firewall:
                                   '[find list={} address={}]'.format(self.address_list_name, ip))
 
     def sync_qos(self):
+        print "QOS"
+
         def resolve_mk(x):
             if x.endswith('k'):
                 return int(x[:-1])
@@ -56,7 +61,6 @@ class Firewall:
         for subnet in IPInterface.objects.filter(iface__nas=self.nas):
             for account in Account.objects.filter(active=True, tariff__qos_speed__gt=0, ip__in=subnet.network):
                 ip = str(account.ip)
-                print ip
                 if ip in mik_qos_dict:
                     if mik_qos_dict[ip][2] != mik_qos_dict[ip][3] or \
                         mik_qos_dict[ip][3] != account.tariff.qos_speed or \
@@ -80,6 +84,35 @@ class Firewall:
             self.nas.exec_command('/queue simple remove '
                                   '[find comment={0} target={1}/32] '.format(self.address_list_name, ip))
 
+
+    def sync_dhcp(self):
+        print "DHCP"
+        stdin, stdout, stderr = self.nas.exec_command('/ip dhcp-server lease print without-paging where dynamic=no')
+
+        mik_dhcp_dict = {}
+        mik_dhcp = stdout.read()
+        mik_dhcp = self.dhcp_re.findall(mik_dhcp)
+        for row in mik_dhcp:
+            mik_dhcp_dict[row[1]] = row[2].lower()
+        for subnet in IPInterface.objects.filter(iface__nas=self.nas):
+            for account in Account.objects.filter(ip__in=subnet.network):
+                if account.ip and account.mac:
+                    ip, mac = str(account.ip), str(account.mac).lower()
+                    if ip in mik_dhcp_dict:
+                        if mac != mik_dhcp_dict[ip]:
+                            print "Update", ip, mac
+                            self.nas.exec_command('/ip dhcp-server lease set '
+                                                  '[find address={}] '
+                                                  'mac-address={}'.format(ip, mac))
+                        del mik_dhcp_dict[ip]
+                    else:
+                        print 'Add', ip, mac
+                        self.nas.exec_command('/ip dhcp-server lease add '
+                                              'address={} mac-address={}'.format(ip, mac))
+        for ip in mik_dhcp_dict:
+            print "Remove", ip
+            self.nas.exec_command('/ip dhcp-server lease remove [find address={}]'.format(ip))
+
     def sync_arp(self):
         pass
 
@@ -87,4 +120,5 @@ class Firewall:
         self.sync_access()
         self.sync_qos()
         self.sync_arp()
+        self.sync_dhcp()
         return True
