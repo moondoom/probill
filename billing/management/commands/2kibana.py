1a5c30df245323ec5b5b165effdfaa6c19945c60
animage <me@animage.me>
2014-09-16 01:32:19 +0700

    asd

--- billing/management/commands/2kibana.py
+++ billing/management/commands/2kibana.py
@@ -15,18 +15,32 @@ class Command(BaseCommand):
         from elasticsearch import Elasticsearch, client, exceptions
         if len(args) == 1:
             command = args[0]
+            tz = pytz.timezone(TIME_ZONE)
             if command == 'export_detail':
                 es = Elasticsearch()
                 es_c = client.IndicesClient(es)
-                es_c.delete(index='traffic_detail')
+                try:
+                    es_c.delete(index='traffic_detail')
+                except exceptions.NotFoundError as e:
+                    print "Not found :)"
+                es_c.create(index='traffic_detail')
+                es_c.put_mapping(index='traffic_detail', doc_type='TrafficDetail', body={
+                    "properties": {
+                        "src_ip": {"type": "string", "index": "not_analyzed"},
+                        "dst_ip": {"type": "string", "index": "not_analyzed"},
+                        "account": {"type": "string", "index": "not_analyzed"}
+                    }
+                })
                 query = TrafficDetail.objects.filter(datetime__gt=(datetime.now() - timedelta(days=1)),
                                                      datetime__lt=datetime.now())
+                query = query.prefetch_related('account')
 
                 for x in query:
                     es.index(index='traffic_detail', doc_type='TrafficDetail', id=x.id, body={
-                        "@timestamp": x.datetime,
+                        "@timestamp": x.datetime - tz.utcoffset(x.datetime),
                         "src_ip": str(x.src_ip),
                         "dst_ip": str(x.dst_ip),
+                        "account": unicode(x.account),
                         "count": x.count
                     })
             if command == 'export_period':
@@ -36,7 +50,6 @@ class Command(BaseCommand):
                     es_c.delete(index='traffic_by_period')
                 except exceptions.NotFoundError as e:
                     print "Not found :)"
-                    pass
                 es_c.create(index='traffic_by_period')
                 es_c.put_mapping(index='traffic_by_period', doc_type='TrafficByPeriod', body={
                     "properties": {
@@ -48,14 +61,14 @@ class Command(BaseCommand):
                 query = TrafficByPeriod.objects.filter(datetime__gt=(datetime.now() - timedelta(days=1)),
                                                        datetime__lt=datetime.now())
                 query = query.prefetch_related("account", "tariff", "qac_class")
-                tz = pytz.timezone(TIME_ZONE)
+
                 for x in query:
                     if x.qac_class:
                         qac_name = unicode(x.qac_class.name)
                     else:
                         qac_name = "External"
                     es.index(index='traffic_by_period', doc_type='TrafficByPeriod', id=x.id, body={
-                        "@timestamp": x.datetime + tz.utcoffset(x.datetime),
+                        "@timestamp": x.datetime - tz.utcoffset(x.datetime),
                         "login": unicode(x.account),
                         "tariff": unicode(x.tariff),
                         "qac": qac_name,
